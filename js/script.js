761056235b41a5eb3f1f4b4b2a8cda482f444463
Dilip raj <dr1117255@gmail.com>
2024-06-09 08:15:11 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+$('.textimonial_items_siders').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    dots:true,
+});
+$('.adventure_sliders').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows:false,
+    dots:false,
+});
+$('.instagram_images').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows:true,
+    dots:false,
+    prevArrow :'<button type="button" class="right"><i class="fa-solid fa-angle-right"></i></button> ',
+    nextArrow :'<button type="button" class="left"><i class="fa-solid fa-angle-left"></i></button> ',
+});
+// $('.client_items_part').slick({
+//     slidesToShow: 5,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 1000,
+//     arrows:false,
+//     dots:false,
+// });
